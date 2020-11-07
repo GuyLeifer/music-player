2409af2599a80736bb77c8def3d6ff5ab7be74d5
GuyLeifer <67858228+GuyLeifer@users.noreply.github.com>
2020-11-07 04:57:32 +0200

    add images to code and style footer

--- client/src/components/about/About.js
+++ client/src/components/about/About.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './About.css';
 
+//icons
+import youtubeIcon from './images/youtubeIcon.jpg';
+
 function About() {
     return (
         <div className="about">
             <h1>
                 About Page
             </h1>
-            <img className="aboutImg" src="https://i.pinimg.com/564x/91/c1/cd/91c1cdeacc84d0a5673bb716549ba366.jpg" alt="About"/>
+            <img className="aboutImg" src={youtubeIcon} alt="About"/>
             <p>This is a Music Streamer Included a DataBase of Songs, Artists, Albums and Playlist.</p>
             <p>Enjoy the Experience of the Streamer!</p>
         </div>
