fda6b37bdcfd78da2df68f66ef2b5f8857d63c74
GuyLeifer <67858228+GuyLeifer@users.noreply.github.com>
2020-11-09 11:37:11 +0200

    add options on home page

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './HomePage.css';
+
+// components
 import TopSongs from './songs/TopSongs';
 import TopArtists from './artists/TopArtists';
 import TopAlbums from './albums/TopAlbums';
 import TopPlaylists from './playlists/TopPlaylists';
 
 function HomePage() {
+
+    const [topOption, setTopOption] = useState("like");
+
     return (
         <div className="homepage">
-            <TopSongs />
-            <TopArtists />
-            <TopAlbums />
-            <TopPlaylists />
+
+            <div className="topTitles"> 
+                <h2 className="topLink" onClick={() => setTopOption("like")}>Top Liked</h2>
+                <h2 className="topLink" onClick={() => setTopOption("play")}>Top Played</h2>
+                <h2 className="topLink" onClick={() => setTopOption("new")}>Newest</h2>
+            </div>
+
+            <TopSongs topOption={topOption}/>
+            <TopArtists topOption={topOption}/>
+            <TopAlbums topOption={topOption}/>
+            <TopPlaylists topOption={topOption}/>
         </div>
     )
 }
